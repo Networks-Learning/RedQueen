122d8be88b4e12eb2906052c2f0d438acbce4006
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-04-26 15:41:01 +0200

    Add implementation of dynamic programming solution.
    But it is buggy/algorithm is not correct.

--- utils.py
+++ utils.py
@@ -46,3 +46,57 @@ def u_int(df, src_id, end_time, q_vec=None, s=1.0, follower_ids=None):
     return np.sum(u_values * u_dt)
 
 
+def oracle_ranking(df, K,
+                   omit_src_ids=None,
+                   follower_ids=None,
+                   q_vec=None,
+                   s=1.0):
+    """Returns the best places the oracle would have put events. Optionally, it
+    can remove sources, use a custom weight vector and have a custom list of
+    followers.."""
+
+    if omit_src_ids is not None:
+        df = df[~df.src_id.isin(omit_src_ids)]
+
+    if follower_ids is not None:
+        df = sorted(df[df.sink_id.isin(follower_ids)])
+    else:
+        follower_ids = df.sink_id.unique()
+
+    if q_vec is None:
+        q_vec = np.ones(len(follower_ids), dtype=float)
+
+    assert is_sorted(df.t.values), "Dataframe is not sorted by time."
+
+    # Find ∫r(t)dt if the Oracle had tweeted once in the beginning and then
+    # had not tweeted ever afterwards.
+
+    # There will be some NaN till the point all the sources had tweeted.
+    # Also, the ranks will start from 0. Fixing that:
+    oracle_rank = rank_of_src_in_df(df, src_id=None, with_time=False).fillna(0)
+
+    # Though each event should have a unique time, we take the 'mean' to catch
+    # bugs which may otherwise go unnoticed if we used .min or .first
+    event_times = df.groupby('event_id').t.mean()
+    event_ids = oracle_rank.index.values
+
+    r_values = oracle_rank[follower_ids].values.dot(np.sqrt(q_vec) / s)
+    r = pd.Series(data=r_values, index=event_ids)
+
+    T = np.inf
+    oracle_event_times = [None] * K
+    K_orig = K
+
+    while K > 0:
+        all_moves = [(r[e_id] * (T - event_times[e_id]), event_times[e_id])
+                     for e_id in event_times.index[event_times < T]]
+        if len(all_moves) == 0:
+            print('Ran out of moves after {} steps'.format(K_orig - K))
+            break
+
+        best_move = sorted(all_moves, reverse=True)[0]
+        K -= 1
+        oracle_event_times[K] = best_move[1]
+        T = best_move[1]
+
+    return oracle_event_times
