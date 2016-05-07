6e9bba9d9c171802ea0ea262a5f64641871d2ad3
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-05-07 21:39:29 +0200

    Move code for simulation into utils.py.

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import sys
 import datetime as D
+from options import Options, optioned
 
 ## Utilities
 
@@ -462,3 +463,317 @@ def sweep_s(sim_opts_gen, capacity_cap, tol=1e-2, verbose=False, s_init=1.0):
     # Step 3: Return
     return s
 
+
+
+## Workers for metrics
+
+import broadcast.opt.optimizer as Bopt
+
+# This is how much after the event that the Oracle tweets.
+oracle_eps = 1e-10
+
+Ks = [1, 3, 5, 10, 20]
+performance_fields = ['seed', 's', 'type'] + ['top_' + str(k) for k in Ks] + ['avg_rank', 'r_2']
+
+def add_perf(op, df, sim_opts):
+    for k in Ks:
+        op['top_' + str(k)] = time_in_top_k(df=df, K=k, sim_opts=sim_opts)
+
+    op['avg_rank'] = average_rank(df, sim_opts=sim_opts)
+    op['r_2'] =  r_2(df, sim_opts=sim_opts)
+
+
+def worker_opt(params):
+    seed, sim_opts, queue = params
+    sim_mgr = sim_opts.create_manager_with_opt(seed=seed)
+    sim_mgr.run()
+    df = sim_mgr.state.get_dataframe()
+    capacity = u_int_opt(df=df, sim_opts=sim_opts)
+    op = {
+        'type': 'Opt',
+        'seed': seed,
+        'capacity': capacity,
+        'sim_opts': sim_opts,
+        's': sim_opts.s,
+        'num_events': np.sum(df.src_id == sim_opts.src_id)
+    }
+
+    add_perf(op, df, sim_opts)
+
+    if queue is not None:
+        queue.put(op)
+
+    return op
+
+
+def worker_poisson(params):
+    seed, capacity, sim_opts, queue = params
+    sim_mgr = sim_opts.create_manager_with_poisson(seed=seed, capacity=capacity)
+    sim_mgr.run()
+    op = {
+        'type': 'Poisson',
+        'seed': seed,
+        'sim_opts': sim_opts,
+        's': sim_opts.s
+    }
+
+    df = sim_mgr.state.get_dataframe()
+
+    add_perf(op, df, sim_opts)
+
+    if queue is not None:
+        queue.put(op)
+
+    return op
+
+
+def worker_oracle(params):
+    seed, capacity, sim_opts, queue = params
+    opt_oracle = find_opt_oracle(capacity, sim_opts)
+    oracle_df = opt_oracle['df']
+    opt_oracle_mgr = sim_opts.create_manager_with_times(oracle_df.t[oracle_df.events == 1] + oracle_eps)
+    opt_oracle_mgr.run()
+    df = opt_oracle_mgr.state.get_dataframe()
+
+    op = {
+        'type': 'Oracle',
+        'seed': seed,
+        'sim_opts': sim_opts,
+        's': sim_opts.s,
+        'r0_num_events': np.sum(oracle_df.events == 1),
+        'num_events': np.sum(df.src_id == sim_opts.src_id)
+    }
+
+
+    add_perf(op, df, sim_opts)
+
+    if queue is not None:
+        queue.put(op)
+
+    return op
+
+
+def worker_kdd(params):
+    seed, capacity, num_segments, sim_opts, world_changing_rates, queue = params
+
+    T = sim_opts.end_time
+    seg_len = T / num_segments
+
+    if world_changing_rates is None:
+        wall_mgr = sim_opts.create_manager_for_wall()
+        wall_mgr.run()
+        wall_df = wall_mgr.state.get_dataframe()
+        seg_idx = (wall_df.t.values / T * num_segments).astype(int)
+        wall_intensities = wall_df.groupby(seg_idx).size() / (T / num_segments)
+    else:
+        wall_intensities = world_changing_rates
+
+    follower_wall_intensities = np.array([wall_intensities])
+    follower_conn_prob = np.asarray([[1.0] * num_segments])
+    follower_weights = [1.0]
+
+    upper_bounds = np.array([1e6] * num_segments)
+    threshold = 0.005
+
+    op = {
+        'type'   : 'kdd',
+        'seed'   : seed,
+        'sim_opts': sim_opts,
+        's'      : sim_opts.s
+    }
+
+    best_avg_rank, best_avg_k = np.inf, -1
+    best_r_2, best_r_2_k = np.inf, -1
+
+    for k in Ks:
+        def _util(x):
+            return Bopt.utils.weighted_top_k(x,
+                                             follower_wall_intensities,
+                                             follower_conn_prob,
+                                             follower_weights,
+                                             k)
+        def _util_grad(x):
+            return Bopt.utils.weighted_top_k_grad(x,
+                                                  follower_wall_intensities,
+                                                  follower_conn_prob,
+                                                  follower_weights,
+                                                  k)
+
+        x0 = np.ones(num_segments)
+
+        kdd_opt = Bopt.optimize(util=_util,
+                                util_grad=_util_grad,
+                                budget=capacity,
+                                upper_bounds=upper_bounds,
+                                threshold=threshold,
+                                x0=x0)
+
+        piecewise_const_mgr = sim_opts.create_manager_with_piecewise_const(
+            seed=seed,
+            change_times=np.arange(num_segments) * seg_len,
+            rates=kdd_opt / seg_len
+        )
+        piecewise_const_mgr.run()
+        df = piecewise_const_mgr.state.get_dataframe()
+        perf = time_in_top_k(df=df, K=K, sim_opts=sim_opts)
+        op['top_' + str(k)] = perf
+
+        avg_rank = average_rank(df, sim_opts=sim_opts)
+        r_2 = int_r_2(df, sim_opts=sim_opts)
+
+        op['avg_rank_' + str(k)] = avg_rank
+        op['r_2_' + str(k)] = r_2
+
+
+        if avg_rank < best_avg_rank:
+            best_avg_rank = avg_rank
+            best_avg_k = k
+
+        if r_2 < best_r_2:
+            best_r_2 = r_2
+            best_r_2_k = k
+
+    op['avg_rank']   = best_avg_rank
+    op['avg_rank_k'] = best_avg_k
+    op['r_2']        = best_r_2
+    op['r_2_k']      = best_r_2_k
+
+    if queue is not None:
+        queue.put(op)
+
+    return op
+
+
+
+# This is an approach using the multiprocessing module without the Pool and using a queue to accumulate the results.
+# This will lead to a better utilization of the CPU resources (hopefully) because the previous method only allowed
+# parallization of the number of seeds.
+
+
+@optioned(option_arg='opts')
+def piecewise_sim_opt_factory(N, T, num_segments, world_rate):
+    random_state = np.random.RandomState(42)
+    world_changing_rates = random_state.uniform(low=world_rate / 2.0, high=world_rate, size=num_segments)
+    world_change_times = np.arange(num_segments) * T / num_segments
+
+    def sim_opts_gen(seed):
+        return SimOpts.std_piecewise_const(world_rates=world_changing_rates,
+                                           world_change_times=world_change_times,
+                                           world_seed=seed + 42).update({'end_time': T })
+
+    return Options(N=N, T=T, num_segments=num_segments, sim_opts_gen=sim_opts_gen)
+
+simulation_opts = Options(world_rate=1000.0, world_alphs=1.0, world_beta=2.0,
+                          N=10, T=1.0, num_segments=10)
+
+poisson_inf_opts = simulation_opts.set_new(
+    sim_opts_gen=lambda seed: SimOpts.std_poisson(world_rate=simulation_opts.world_rate,
+                                                  world_seed=seed))
+piecewise_inf_opts = piecewise_sim_opt_factory(opts=simulation_opts)
+hawkes_inf_opts = simulation_opts.set_new(
+    sim_opts_gen=lambda seed: SimOpts.std_hawkes(world_seed=seed,
+                                                 world_lambda_0=simulation_opts.world_rate,
+                                                 world_alpha=simulation_opts.world_alpha,
+                                                 world_beta=simulation_opts.world_beta))
+
+@optioned(option_arg='opts')
+def run_inference(N, T, num_segments, sim_opts_gen):
+
+    def extract_perf_fields(return_obj):
+        """Extracts the relevant fields from the return object and returns them in a new dict."""
+        result_dict = {}
+        for field in performance_fields:
+            result_dict[field] = return_obj[field]
+
+        return result_dict
+
+    processes = []
+    queue = mp.Queue()
+    results = []
+    capacities = {}
+    raw_results = []
+
+    try:
+        active_processes = 0
+        for s in np.logspace(-8, 1, num=10):
+            capacities[s] = []
+            for seed in range(N):
+                active_processes += 1
+                sim_opts = sim_opts_gen(seed).update({ 's' : s })
+                p = mp.Process(target=worker_opt,
+                               args=((seed, sim_opts, queue),))
+                processes.append(p)
+                p.daemon = True
+                p.start()
+
+        logTime('Started all processes: {}'.format(active_processes))
+
+        while active_processes > 0:
+            # logTime('active_processes = {}'.format(active_processes))
+            r = queue.get()
+            raw_results.append(r)
+            results.append(extract_perf_fields(r))
+
+            if r['type'] == 'Opt':
+                active_processes -= 1
+
+                seed = r['seed']
+                capacity = r['capacity']
+                s = r['sim_opts'].s
+                sim_opts = r['sim_opts']
+                capacities[s].append(capacity)
+
+                # Poisson
+
+                p = mp.Process(target=worker_poisson, args=((seed, capacity, sim_opts, queue),))
+                processes.append(p)
+                p.daemon = True
+                p.start()
+                active_processes += 1
+
+                # Oracle
+
+                oracle_args = (seed, capacity, sim_opts, queue)
+                p = mp.Process(target=worker_oracle, args=(oracle_args,))
+                processes.append(p)
+                p.daemon = True
+                p.start()
+                active_processes += 1
+
+                # KDD solution
+
+                # kdd_args = (seed, capacity, num_segments, sim_opts, world_changing_rates, queue)
+                kdd_args = (seed, capacity, num_segments, sim_opts, None, queue)
+                p = mp.Process(target=worker_kdd, args=(kdd_args,))
+                processes.append(p)
+                p.daemon = True
+                p.start()
+                active_processes += 1
+
+            elif r['type'] == 'Poisson':
+                active_processes -= 1
+                if active_processes % 10 == 0:
+                    logTime('Active processes = {}'.format(active_processes))
+
+            elif r['type'] == 'Oracle':
+                active_processes -= 1
+                if active_processes % 10 == 0:
+                    logTime('Active processes = {}'.format(active_processes))
+
+            elif r['type'] == 'kdd':
+                active_processes -= 1
+                if active_processes % 10 == 0:
+                    logTime('Active processes = {}'.format(active_processes))
+
+            else:
+                raise ValueError('Unknown type: {}'.format(r['type']))
+    finally:
+        # Attempt at cleanup
+        print("Cleaning up {} processes".format(len(processes)))
+        for p in processes:
+            p.terminate()
+            p.join()
+
+    return Options(df=pd.DataFrame.from_records(results),
+                   raw_results=raw_results,
+                   capacities=capacities)
