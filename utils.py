08e9562e38e1482975d1641c64e513d565c362f7
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-05-17 11:58:39 +0200

    Add data saving functions.
    Make SimOpts agnostic to the save format.

--- utils.py
+++ utils.py
@@ -1405,3 +1405,27 @@ def run_real_queue(N, num_segments, files_user_ids, min_user_capacity=2, num_pro
 
 
 
+### Reading real data
+
+base = '/NL/ghtorrent/work/opt-broadcast'
+import os
+import pickle
+import re
+
+user_data_regex = re.compile(r'user-([0-9]*)\.pickle')
+sizes_files = sorted([(os.path.getsize(os.path.join(base, x)), x) for x in os.listdir(base) if user_data_regex.match(x)])
+file_user_id = [(os.path.join(base, x), int(user_data_regex.match(x).group(1))) for _, x in sizes_files]
+
+
+## Saving raw_data
+
+def change_sim_opts(d):
+    d2 = d.copy()
+    del d2['sim_opts']
+    d2['sim_opts_dict'] = d['sim_opts'].get_dict()
+    return d2
+
+def get_savable_raw_results(r):
+    return [change_sim_opts(d) for d in r]
+
+
