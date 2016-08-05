d46bc85a55e132f008046219083be482ac87938b
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-08-05 15:38:50 +0200

    Add code for calculating significance for real-users.

--- real_data_gen.py
+++ real_data_gen.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import broadcast.data.user_repo    as user_repo
 import broadcast.data.db_connector as db_connector
+import numpy                       as np
+import decorated_options           as Deco
 import broadcast.data.hdfs         as hdfs
 import logging
 # Comment out while running from IPython to avoid reloading the module
@@ -9,6 +11,13 @@ from utils import is_sorted, logTime, def_q_vec
 
 scaled_period = 10000.0
 verbose = False
+
+
+def get_start_end_time():
+    return Deco.Options(
+        start_time = 1246406400, # GMT: Wed, 01 Jul 2009 00:00:00 GMT
+        end_time = 1251763200 # GMT: Tue, 01 Sep 2009 00:00:00 GMT
+    )
 
 log = logTime if verbose else lambda *args, **kwargs: None
 
@@ -97,8 +106,9 @@ def get_user_data_for(user_id):
         # Is 2 months now.
 
         first_tweet_time, last_tweet_time = user_tweet_times[0], user_tweet_times[-1]
-        start_time = 1246406400 # GMT: Wed, 01 Jul 2009 00:00:00 GMT
-        end_time = 1251763200 # GMT: Tue, 01 Sep 2009 00:00:00 GMT
+        exp_times = get_start_end_time()
+        start_time = exp_times.start_time # GMT: Wed, 01 Jul 2009 00:00:00 GMT
+        end_time = exp_times.end_time # GMT: Tue, 01 Sep 2009 00:00:00 GMT
 
         if last_tweet_time < start_time:
             # This user did not tweet in the relevant period of time
@@ -157,6 +167,69 @@ def get_user_data_for(user_id):
     finally:
         hs.close()
 
+
+@Deco.optioned('opts')
+def find_significance(user_id,
+                      user_repository,
+                      num_segments=24,
+                      segment_length=60*60,
+                      return_tweet_times=False):
+    # 1. Find all the followers
+    # 2. Find tweet times of the followers
+    # 3. Fit them in num_segments - per day.
+
+    experiment_times = get_start_end_time()
+    start_time = experiment_times.start_time # GMT: Wed, 01 Jul 2009 00:00:00 GMT
+
+    followee_threshold = 500
+
+    user_followers = user_repository.get_user_followers(user_id)
+
+    if len(user_followers) > followee_threshold:
+        logging.error('Number of followers is more than 500.')
+        return None
+
+    follower_significance = []
+    all_tweet_times = []
+
+    time_period = num_segments * segment_length
+
+    # Not sorting the users here to keep the same order as was recorded for the user initially
+    for idx, follower_id in enumerate(user_followers):
+        num_followees_of_follower = len(user_repository.get_user_followees(follower_id))
+
+        if num_followees_of_follower < followee_threshold:
+            follower_tweet_times = user_repository.get_user_tweets(follower_id)
+            # Only use times before start_time
+            follower_tweet_times = follower_tweet_times[follower_tweet_times < start_time]
+
+            follower_tweet_bins = [0] * num_segments
+            if return_tweet_times:
+                all_tweet_times.append(follower_tweet_times)
+
+            for tweet_time in follower_tweet_times:
+                idx = int(num_segments * ((tweet_time - start_time) % time_period) / time_period)
+                follower_tweet_bins[idx] += 1
+
+            follower_significance.append(follower_tweet_bins)
+
+    raw_significance=np.asarray(follower_significance)
+    significance = raw_significance / raw_significance.sum(1)[:,None]
+    significance[np.isnan(significance)] = 1.0 / num_segments
+
+    ret = Deco.Options(
+        raw_significance=raw_significance,
+        significance=significance,
+        total_followers=len(user_followers)
+    )
+
+    if return_tweet_times:
+        ret = ret.set_new(all_tweet_times=all_tweet_times)
+
+    return ret
+
+
+# ------------------------------------------------------------------------
 
 output_folder = '/NL/ghtorrent/work/opt-broadcast/'
 
